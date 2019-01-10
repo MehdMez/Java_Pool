f95e03f9d51d077c5f3a0d4948333d7e5d59b038
MehdMez <mehdimezioud@gmail.com>
2019-01-10 11:21:24 +0100

    task02

---
+++ JavaDay06/task02/DataServer.java
@@ -0,0 +1,34 @@
+import java.net.*;
+import java.io.*;
+
+public class DataServer{
+    public static void main(String args[]) throws Exception{
+	ServerSocket servsock = new ServerSocket(3333);
+	System.out.println("Ok, ready");
+	Socket sock = servsock.accept();
+
+	//Declaration pour lecture de l'input clavier
+	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	//Envoi au client grace au writer (attention au type)
+	OutputStream ostream = sock.getOutputStream();
+	PrintWriter writer = new PrintWriter(ostream, true);
+	writer.write("Welcome to my server !");
+	writer.flush();
+
+	//Reception de message grace au reciever
+	InputStream istream = sock.getInputStream();
+	BufferedReader reciever = new BufferedReader(new InputStreamReader(istream));
+
+	String messageRecu, messageEnvoye;
+	//while(true) -> trant que le serveur tourne
+	while(true){
+	    if((messageRecu = reciever.readLine()) != null){
+		System.out.println("Message recieved : "+messageRecu);
+	    }
+	    messageEnvoye = reader.readLine();
+	    writer.println(messageEnvoye);
+	    writer.flush();  //NEcessaire pour le prochain input 
+	}	
+    }
+}
+	
