7cc90181efc29618c7cf810333663383e714eb8c
MehdMez <mehdimezioud@gmail.com>
2019-01-11 10:01:58 +0100

    task01

---
+++ JavaDay07/task01/CreateWindow.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class CreateWindow{
+    private static void createWindow(){
+	//Formule magique apparemment indispensable pour l'utilisation de Swing
+	JFrame.setDefaultLookAndFeelDecorated(true);
+
+	//Creation de la frame
+	JFrame frame = new JFrame("My Frame");
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+
+	//Affichage dans la fenetre
+	JLabel message = new JLabel("Hello World !");
+	frame.getContentPane().add(message); //Ajout à la fenêtre
+
+	//Affichage de la fenetre
+	frame.pack();
+	frame.setVisible(true);
+    }
+
+    public static void main(String[] args){
+	createWindow();
+    }
+}
